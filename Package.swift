5b9d833eaa0284fedd17717d70079bfd14e7192e
Greg Hughes <com.greghughes.MemeKeyboard>
2019-10-30 10:24:37 -0600

    4

--- Package.swift
+++ Package.swift
@@ -7,20 +7,9 @@ let package = Package(
     name: "objcTest",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(
-            name: "objcTest",
-            targets: ["objcTest"]),
-        .library(
-        name: "Epson",
-        targets: ["objcTest"]),
-        .library(
-        name: "libepos2",
-        targets: ["objcTest"]),
+      
+        .library(name: "objcTest", type: .static, targets: ["objcTest"])
         
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -28,13 +17,9 @@ let package = Package(
 
         .target(
             name: "objcTest",
-            dependencies: []),
+            dependencies: ["Epson"]),
         .target(
-        name: "Epson",
-        dependencies: []),
-        .target(
-        name: "libepos2",
-        dependencies: []),
+        name: "Epson"),
         
         .testTarget(
             name: "objcTestTests",
---
+++ Sources/Epson/Epson.m
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Greg Hughes on 10/30/19.
+//
+
+#include "Epson.h"
